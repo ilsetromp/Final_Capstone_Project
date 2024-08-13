87c38ecfa7c165de91c18d89c3db1dc254a3e9ae
Dennis Tromp <dennistromp@MacBook-Air-van-Dennis.local>
2024-08-13 10:36:39 +0200

    Final adjustments

--- Choose_Your_Own.R
+++ Choose_Your_Own.R
@@ -1,11 +1,20 @@
+if (!require(e1071)) install.packages("dplyr")
+if (!require(e1071)) install.packages("tidyverse")
+if (!require(e1071)) install.packages("caret")
+if (!require(e1071)) install.packages("ggplot2")
+if (!require(e1071)) install.packages("rpart")
+if (!require(e1071)) install.packages("rpart.plot")
+if (!require(e1071)) install.packages("randomForest")
+if (!require(e1071)) install.packages("e1071")
+
 library("dplyr")
 library("tidyverse")
 library("caret")
 library("ggplot2")
 library("rpart")
 library("rpart.plot")
-library("e1071")
 library("randomForest")
+library("e1071")
 
 # Introduction
 
@@ -18,7 +27,7 @@ library("randomForest")
 #The participants were aged between 19 and 48 years of age and each participant performed 6 daily activities. 
 #These activities were: walking, walking downstairs, sitting, standing, and laying. 
 #The sampling rate of the recordings was 50Hz and stored as time series per dimension. 
-#So, six different signals were measured: 3 from the accerlerometer and 3 from the gyroscope.  
+#So, six different signals were measured: 3 from the accelerometer and 3 from the gyroscope.  
 
 #The data set has already been divided into training and test sets. 
 #Of the participants, 70% was randomly selected to form the training set, 
@@ -27,41 +36,52 @@ library("randomForest")
 #In this project, I will develop a model that determines the type of activity, 
 #based on data collected by smartphone sensors. 
 
-# Loading the data
 
-# Load and prepare the data
+# Loading and preparing the data
+
+# Loading the train data
 X_train <- read.table("X_train.txt")
 y_train <- read.table("y_train.txt")
 subject_train <- read.table("subject_train.txt")
 
+# Loading the test data
 X_test <- read.table("X_test.txt")
 y_test <- read.table("y_test.txt")
 subject_test <- read.table("subject_test.txt")
 
+# Loading features and activity labels
 features <- read.table("features.txt")
 feature_names <- as.character(features$V2)
 activity_labels <- read.table("activity_labels.txt")
+
+# Assigning an ID and name to activities
 colnames(activity_labels) <- c("ActivityID", "ActivityName")
 
+# Assigning feature names to the X variables
 colnames(X_train) <- feature_names
 colnames(X_test) <- feature_names
 
+# Assigning column names to the y and subject data
 colnames(y_train) <- "Activity"
 colnames(subject_train) <- "Subject"
 colnames(y_test) <- "Activity"
 colnames(subject_test) <- "Subject"
 
+# Combining train data into one
 train_data <- cbind(subject_train, y_train, X_train)
+
+# Combining test data into one
 test_data <- cbind(subject_test, y_test, X_test)
 
-# Check for duplicate columns and rename if necessary
+# Checking for duplicate columns and rename if necessary
 colnames(train_data) <- make.names(colnames(train_data), unique = TRUE)
 colnames(test_data) <- make.names(colnames(test_data), unique = TRUE)
 
+# Transforming activity into a categorical variable
 train_data$Activity <- factor(train_data$Activity, levels = activity_labels$ActivityID, labels = activity_labels$ActivityName)
 test_data$Activity <- factor(test_data$Activity, levels = activity_labels$ActivityID, labels = activity_labels$ActivityName)
 
-# Combining all data into one data frame
+# Combining all data into one data frame for data exploration
 combined_data <- cbind(subject_data, y_data, X_data)
 
 # Before we start the data exploration, let's check for missing values
@@ -75,35 +95,35 @@ sum(is.na(combined_data))
 # Distribution of activities
 table(combined_data$Activity)
 
-# Check for duplicate column names
+# Checking for duplicate column names
 duplicate_columns <- duplicated(colnames(combined_data))
 print(duplicate_columns)
 
-# Print the names of duplicate columns
+# Showing duplicate column names
 duplicate_column_names <- colnames(combined_data)[duplicate_columns]
-print(duplicate_column_names)
+duplicate_column_names
 
 # Renaming duplicates
 colnames(combined_data) <- make.names(colnames(combined_data), unique = TRUE)
 
-# Plot the data again
 # Bar plot of activity distribution
 ggplot(combined_data, aes(x = Activity)) +
   geom_bar() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   labs(title = "Activity Distribution", x = "Activity", y = "Count")
 
-# Boxplot of a sample feature for each activity
-ggplot(combined_data, aes(x = Activity, y = combined_data[, 3])) +
-  geom_boxplot() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  labs(title = "Boxplot of Feature 1 by Activity", x = "Activity", y = colnames(combined_data)[3])
+# Methods
 
-
-# Model development
+# To predict the right activity based on the smartphone sensor data, 
+# I will use several models and test the accuracy of each. 
+# First, I will develop a baseline model to compare the results to. 
 
 # Baseline model
-# Decistion tree
+# The baseline model I will use, is a decision tree. 
+# A decision tree model splits the data its given into smaller sets at each decision node.
+# This way, what's left in the end is hopefully the type of activity that corresponds to the data. 
+
+# Decision tree
 
 # Fitting the decision tree model
 tree_model <- rpart(Activity ~ ., data = train_data, method = "class")
@@ -118,11 +138,27 @@ tree_predictions <- predict(tree_model, test_data, type = "class")
 tree_conf_matrix <- confusionMatrix(tree_predictions, test_data$Activity)
 
 # Showing the confusion matrix
-print(tree_conf_matrix)
+tree_conf_matrix
 
-# Random Forests
-# Train the Random Forest model
+# Calculating accuracy
+tree_accuracy <- as.numeric(tree_conf_matrix$overall['Accuracy'])
+
+# Showing a table with the model and its corresponding accuracy
+accuracy_table <- data.frame(Model = "Decision Tree", Accuracy = tree_accuracy)
+accuracy_table
+
+# The first model I will test and compare to the baseline model, is random forest. 
+# Random forests is a model that is made up of several decision trees. 
+# In this case, the variable needs to be classified. 
+# I.E., the type of activity is determined. 
+# So, in the end the most frequent activity type over all decision trees, will be predicted. 
+
+
+# Random Forest
+
 set.seed(123)  # For reproducibility
+
+# Training the Random Forest model
 rf_model <- randomForest(Activity ~ ., data = train_data, ntree = 100, importance = TRUE)
 
 rf_model
@@ -132,11 +168,73 @@ rf_predictions <- predict(rf_model, test_data)
 
 # Confusion matrix and accuracy
 rf_conf_matrix <- confusionMatrix(rf_predictions, test_data$Activity)
-print(rf_conf_matrix)
+rf_conf_matrix
 
-# Print the overall accuracy
-cat("Accuracy: ", rf_conf_matrix$overall['Accuracy'], "\n")
+# Showing the overall accuracy
+rf_accuracy <- cat("Accuracy: ", rf_conf_matrix$overall['Accuracy'], "\n")
+rf_accuracy <- as.numeric(rf_conf_matrix$overall['Accuracy'])
 
-
+# Showing a table with accuracies from both the decision tree and RF models
+accuracy_table <- rbind(accuracy_table, data.frame(Model = "Random Forest", Accuracy = rf_accuracy))
+accuracy_table
 
 # Support Vector Machines
+
+# The next and last model we will test is the Support Vector Machines (SVM) model.
+# SVM tries to find a hyperplane that separates the points of different classes.
+# SVM then tries to find the hyperplane that has the largest distance between the points from the different classes. 
+
+# Setting seed for reproducibility
+set.seed(123)
+
+# Training the SVM model with a linear kernel
+svm_model <- svm(Activity ~ ., data = train_data, kernel = "linear", scale = TRUE)
+
+svm_model
+
+# Predicting on the test set
+svm_predictions <- predict(svm_model, test_data)
+
+# Confusion matrix and accuracy
+svm_conf_matrix <- confusionMatrix(svm_predictions, test_data$Activity)
+svm_conf_matrix
+
+# Showing the overall accuracy
+svm_accuracy <- cat("Accuracy: ", svm_conf_matrix$overall['Accuracy'], "\n")
+svm_accuracy <- as.numeric(svm_conf_matrix$overall['Accuracy'])
+
+# Showing a table with accuracies from all models
+accuracy_table <- rbind(accuracy_table, data.frame(Model = "SVM", Accuracy = svm_accuracy))
+accuracy_table
+
+# Results
+
+accuracy_table
+
+# Conclusion
+
+# The baseline model already worked quite well, with an accuracy of 0.840.
+# The RF model was a good improvement with an accuracy of 0.928. 
+# And the SVM model worked best, with an accuracy of 0.957.
+
+# This project has some limitations.
+# First of all, the data was recorded from only 30 participants. 
+# This could cause, that, when these models would be used on "real world" data,
+# It could be less precise in predicting activity type correctly. 
+# Furthermore, the activity types were recorded in a controlled setting. 
+# Again, when "real world" data would be used, It could be quite noisy and hard to predict. 
+# Lastly, although RF and SVM seem like the right models, 
+# these could still have overfitted the data. 
+# This would mean, that although these models seem to work good on the test data, 
+# they might not on real data. 
+
+
+# References
+
+# Reyes-Ortiz,Jorge, Anguita,Davide, Ghio,Alessandro, Oneto,Luca, and Parra,Xavier. (2012). Human Activity Recognition Using Smartphones. UCI Machine Learning Repository. https://doi.org/10.24432/C54S4K.
+
+# R.A. Irizarry. (2019). Introduction to Data Science.
+
+# IBM. "What Is Random Forest?" IBM, https://www.ibm.com/topics/random-forest#:~:text=Random%20forest%20is%20a%20commonly,both%20classification%20and%20regression%20problems. Accessed 13 Aug. 2024.
+
+# GeeksforGeeks. "Support Vector Machine Algorithm." GeeksforGeeks, https://www.geeksforgeeks.org/support-vector-machine-algorithm/. Accessed 13 Aug. 2024.
